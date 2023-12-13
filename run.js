2a751ba164479080430fe2eeb6fc977504b36b87
Sarah Reichelt <sarah@troz.net>
2023-12-13 16:15:00 +1000

    day 13 part 1

--- run.js
+++ run.js
@@ -1,7 +1,7 @@
 const { readFile } = require('./utils')
 
-const dayNum = 12
-const expectedTestResult1 = 21
+const dayNum = 13
+const expectedTestResult1 = 405
 const expectedTestResult2 = 0
 
 const { runPart1, runPart2 } = require(`./scripts/day${dayNum}`)
--- scripts/day13.js
+++ scripts/day13.js
@@ -1,8 +1,91 @@
 const { convertToIntegers } = require('../utils')
 
-const runPart1 = input => {}
+const runPart1 = input => {
+  const patterns = input.join('\n').split('\n\n')
 
-const runPart2 = input => {}
+  let lefts = 0
+  let aboves = 0
+
+  for (const pattern of patterns) {
+    let patternUp = lookForHorizontalMirror(pattern)
+    if (patternUp) {
+      aboves += patternUp
+    }
+    let patternLeft = lookForVerticalMirror(pattern)
+    if (patternLeft) {
+      lefts += patternLeft
+    }
+  }
+
+  const result = aboves * 100 + lefts
+  return result
+
+  // 27664
+}
+
+const runPart2 = input => {
+  // Change basic checks
+  // compare line 1 & 2
+  // compare line 1 + 2 to 3 + 4
+  // compare line 1 + 2 + 3 to 4 + 5 + 6
+  // then if the combined strings differ by only 1, you've found the smudge
+}
+
+const lookForHorizontalMirror = (pattern, direction = 'H') => {
+  // find two lines that are the same
+  // the expand out in both directions and check for matches until one side reaches the end
+
+  const rows = pattern.split('\n')
+  for (let r = 1; r < rows.length; r++) {
+    if (rows[r] === rows[r - 1]) {
+      let up = r - 1
+      let down = r
+      let match = true
+
+      while (match) {
+        up--
+        down++
+
+        if (up < 0 || down >= rows.length) {
+          break
+        }
+
+        const upperLine = rows[up]
+        const lowerLine = rows[down]
+        if (upperLine !== lowerLine) {
+          match = false
+        }
+      }
+
+      if (match) {
+        return r
+      }
+    }
+  }
+
+  return undefined
+}
+
+const lookForVerticalMirror = pattern => {
+  let cols = []
+  const rows = pattern.split('\n')
+  for (let r = 0; r < rows.length; r++) {
+    for (let c = 0; c < rows[r].length; c++) {
+      if (!cols[c]) {
+        cols[c] = []
+      }
+      cols[c][r] = rows[r][c]
+    }
+  }
+
+  let rotatedPattern = []
+  for (const c of cols) {
+    rotatedPattern.push(c.join(''))
+  }
+
+  const mirror = lookForHorizontalMirror(rotatedPattern.join('\n'), 'V')
+  return mirror
+}
 
 module.exports = {
   runPart1,
